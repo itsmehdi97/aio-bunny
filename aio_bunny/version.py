f718e3e03d0c49c80487e4f6b902cd9e0fa8aca5
mehdi <m.kamani@sheypoor.com>
2024-11-25 20:39:01 +0330

    bump to 0.1.0

--- aio_bunny/version.py
+++ aio_bunny/version.py
@@ -1,15 +1,12 @@
 author_info = (
-    ("Author", "mkamani@sheypoor.com"),)
+    ("Author", "itsmehdi97@gmail.com"),)
 
-package_info = """
-An asyncio wrapper around aio-pika.
-Provides simplified API, graceful consumer shutdown and optional idempotency.
-"""
+package_info = "asyncio compatible wraper around the aio-pika for consumining/publishing Rabbitmq messages."
 package_license = "MIT License"
 
 team_email = ""
 
-version_info = (0, 0, 7)
+version_info = (0, 1, 0)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
